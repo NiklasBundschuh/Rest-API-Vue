184bb9096834bc4e675effc22cbb4e8f26f4da03
NiklasBundschuh <78409354+NiklasBundschuh@users.noreply.github.com>
2021-02-16 10:31:32 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Hallo</h1>
+    
+    
+  </div>
+</template>
+
+<script>
+
+import OutputList from './components/OutputList'
+export default {
+  name: 'App',
+  components: {
+    OutputList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ OutputList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Output List</h1>
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+export default {
+    name:"output-list",
+    mounted()
+    {
+        Vue.axios.get('http://127.0.0.1:1337/multi/55')
+        .then((resp)=>{
+            console.warn(resp)
+        })
+    }
+}
+</script>
